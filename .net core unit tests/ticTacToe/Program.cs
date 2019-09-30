7cdcbd2437858fc1016c34d448180204e62bc252
Silvester <silvester_schneider@gmx.de>
2019-09-30 22:32:50 +0200

    new project for unittests

---
+++ .net core unit tests/ticTacToe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ticTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
